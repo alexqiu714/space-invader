6099077b990efd35413bae2bc5f8a20c4941dcc5
alexqiu714 <haode.qiu@isbasel.ch>
2026-04-21 17:55:56 +0200

    Add files via upload

--- space invaders.py
+++ space invaders.py
@@ -4,6 +4,10 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 500
 FPS = 60
+font = pygame.font.SysFont("arial", 30)
+
+yellowbullet = []
+redbullet = []
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -21,9 +25,27 @@ border = pygame.Rect(390, 0, 20, 500)
 yrec = pygame.Rect(40, 245, 50, 50)
 rrec = pygame.Rect(710, 245, 50, 50)
 
+bulletfiring = pygame.mixer.Sound("Lesson 4 - Space Invaders/images/Gun+Silencer.mp3")
+
 def yellowmove():
     if keys_pressed[pygame.K_w] and yrec.y > 0:
         yrec.y -= 2
+    if keys_pressed[pygame.K_a] and yrec.x > 0: 
+        yrec.x -= 2
+    if keys_pressed[pygame.K_d] and yrec.x < 340: 
+        yrec.x += 2
+    if keys_pressed[pygame.K_s] and yrec.y < 450: 
+        yrec.y += 2
+
+def redmove():
+    if keys_pressed[pygame.K_UP] and rrec.y > 0:
+        rrec.y -= 2
+    if keys_pressed[pygame.K_LEFT] and rrec.x > 410: 
+        rrec.x -= 2
+    if keys_pressed[pygame.K_RIGHT] and rrec.x < 750: 
+        rrec.x += 2
+    if keys_pressed[pygame.K_DOWN] and rrec.y < 450: 
+        rrec.y += 2
 
 clock = pygame.time.Clock()
 
@@ -35,7 +57,18 @@ while True:
     screen.blit(red2, rrec)
     keys_pressed = pygame.key.get_pressed()
     yellowmove()
+    redmove()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LALT:
+                bulletfiring.play()
+                bullet = pygame.Rect(yrec.x + 50, yrec.y + 22, 10, 5)
+                yellowbullet.append(bullet)
+    for i in yellowbullet:
+        pygame.draw.rect(screen, "yellow", bullet)
+        i.x += 10
+    yellowhealthtext = font.render("Health: 10", True, "black")
+    screen.blit(yellowhealthtext, (10, 10))
     pygame.display.update()
