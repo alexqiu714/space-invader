eb865f55d87ba7758ab21aae085d71ffc31448ff
alexqiu714 <haode.qiu@isbasel.ch>
2026-04-28 17:47:50 +0200

    Add files via upload

--- space invaders.py
+++ space invaders.py
@@ -16,6 +16,8 @@ screen.fill("black")
 space = pygame.image.load ("Lesson 4 - Space Invaders/images/space.png")
 red = pygame.image.load ("Lesson 4 - Space Invaders/images/spaceship_red.png")
 yellow = pygame.image.load ("Lesson 4 - Space Invaders/images/spaceship_yellow.png")
+yhealth = 10
+rhealth = 10
 
 yellow1 = pygame.transform.scale(yellow, (50, 50))
 yellow2 = pygame.transform.rotate(yellow1, 90)
@@ -26,6 +28,7 @@ yrec = pygame.Rect(40, 245, 50, 50)
 rrec = pygame.Rect(710, 245, 50, 50)
 
 bulletfiring = pygame.mixer.Sound("Lesson 4 - Space Invaders/images/Gun+Silencer.mp3")
+collide = pygame.mixer.Sound("Lesson 4 - Space Invaders/images/Grenade+1.mp3")
 
 def yellowmove():
     if keys_pressed[pygame.K_w] and yrec.y > 0:
@@ -66,9 +69,40 @@ while True:
                 bulletfiring.play()
                 bullet = pygame.Rect(yrec.x + 50, yrec.y + 22, 10, 5)
                 yellowbullet.append(bullet)
+            if event.key == pygame.K_RALT:
+                bulletfiring.play()
+                bullet = pygame.Rect(rrec.x - 10, rrec.y + 22, 10, 5)
+                redbullet.append(bullet)
     for i in yellowbullet:
         pygame.draw.rect(screen, "yellow", bullet)
         i.x += 10
-    yellowhealthtext = font.render("Health: 10", True, "black")
+        if i.colliderect(rrec):
+            rhealth -= 1
+            collide.play()
+            yellowbullet.remove(i)
+    yellowhealthtext = font.render("Health: " + str (yhealth), True, "black")
     screen.blit(yellowhealthtext, (10, 10))
+    if rhealth <= 0:
+        screen.fill("yellow")
+        gameover = font.render("Game over, yellow wins!", True, "blue")
+        screen.blit(gameover, (255, 20))
+        pygame.display.update()
+        pygame.time.delay(3000)
+        pygame.quit()
+    for i in redbullet:
+        pygame.draw.rect(screen, "red", bullet)
+        i.x -= 10
+        if i.colliderect(yrec):
+            yhealth -=1
+            collide.play()
+            redbullet.remove(i)
+    redhealthtext = font.render("Health: " + str (rhealth), True, "black")
+    screen.blit(redhealthtext, (650, 10))
+    if yhealth <= 0:
+        screen.fill("yellow")
+        gameover = font.render("Game over, red wins!", True, "blue")
+        screen.blit(gameover, (255, 20))
+        pygame.display.update()
+        pygame.time.delay(3000)
+        pygame.quit()
     pygame.display.update()
